99337b8bed7c6cfc0b6e85bb3b7ff03d56b038a2
Jake Barlow <jake@jakebarlow.me>
2020-03-19 15:07:40 +1100

    almost working postgres load

---
+++ sql_functions.py
@@ -0,0 +1,83 @@
+import sqlconfig #psql settings file
+import psycopg2 #psqldumping
+
+def connect():
+	conn = None
+	try:
+		conn = psycopg2.connect(host=sqlconfig.host,database=sqlconfig.database, user=sqlconfig.username, password=sqlconfig.password)
+		cur=conn.cursor()
+		print('PostgreSQL database version:')
+		cur.execute('SELECT version()')
+		db_version = cur.fetchone()
+		print(db_version)
+		return conn
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+
+
+def disconnect(conn):
+	conn.close()
+	print('Database connection closed.')
+
+
+
+def create_tables():
+	try:
+		conn
+		print("Already connected")
+	except NameError:
+		conn = connect()
+		print("Now connected")
+	commands = (
+		"""
+		CREATE TABLE hospitals (
+			id serial NOT NULL PRIMARY KEY,
+			title varchar,
+			metro bool,
+			private bool,
+			tertiary bool,
+			area char(3)
+		);
+		""",
+		"""
+		CREATE TABLE bedstate (
+			id serial NOT NULL PRIMARY KEY,
+			title varchar,
+			icu_occ smallint,
+			icu_emp smallint,
+			icu_aw_adm smallint,
+			icu_aw_dc smallint,
+			hdu_occ smallint,
+			hdu_emp smallint,
+			hdu_aw_adm smallint,
+			hdu_aw_dc smallint,
+			min_icu_eqv smallint,
+			updated varchar,
+			ts timestamptz
+		);
+		"""
+		)
+	cur = conn.cursor()
+	for command in commands:
+		cur.execute(command)
+	cur.close()
+	conn.commit()
+	print('Tables created.')
+	return conn
+
+
+def insert_values():
+	conn = connect()
+	sql = "INSERT INTO bedstate (title, icu_occ, icu_emp, icu_aw_adm, icu_aw_dc, hdu_occ, hdu_emp, hdu_aw_adm, hdu_aw_dc, min_icu_eqv, updated, ts) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+	data = 
+	
+
+#	str(INSERT INTO bedstate (title, icu_occ, icu_emp, icu_aw_adm, icu_aw_dc, hdu_occ, hdu_emp, hdu_aw_adm, hdu_aw_dc, min_icu_eqv, updated, ts)
+#		VALUES(cells[0].find("span").get_text(), cells[2].find("span").get_text(), cells[1].find("span").get_text(), cells[3].find("span").get_text(), cells[4].find("span").get_text(), cells[6].find("span").get_text(), cells[5].find("span").get_text(), cells[7].find("span").get_text(), cells[8].find("span").get_text(), cells[9].find("span").get_text(), cells[10].find("span").get("data-tooltip"), str(dtg));
+#		)
+#	cur=con.cursor()#	disconnect(conn)
+
+
+create_tables()
+disconnect(conn)
+
--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
-import requests
-import time
-import json
+import time #for rendering delay
+from datetime import datetime #for timestamping
+import json #for dumping results ?delete with working psql
+import sql_functions #local set of instructions for updating PSQL DB
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,6 +10,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless") #comment out for debugging
 driver = webdriver.Chrome(options=chrome_options)
 
+dtg = datetime.utcnow()
 
 #ICU Occupancy Data
 ##Victorian Bedstate
@@ -19,37 +21,79 @@ soup = BeautifulSoup(driver.page_source, features="html.parser").find(id="dashbo
 hospitals = soup.find_all('tr', attrs={"data-row-name": "CampusBedNumbers"})
 
 bedstate = []
+hospital_static = []
 
-for hospital in hospitals:
-	cells = hospital.find_all("td")
-	if cells[0].parent['class'][0] == "care-Tertiary":
-		tertiary, metro, public = 1, 1, 1
-	elif cells[0].parent['class'][0] == "care-SubTertiary":
-		tertiary, metro, public = 0, 1, 1
-	elif cells[0].parent['class'][0] == "care-Regional":
-		tertiary, metro, public = 0, 0, 1
-	elif cells[0].parent['class'][0] == "care-Private":
-		tertiary, metro, public = 0, 0, 0
-	hospital_state = {
-		"name": cells[0].find("span").get_text(),
-		"tertiary": tertiary,
-		"metro": metro,
-		"public": public,
-		"icu_empty": cells[1].find("span").get_text(),
-		"icu_occupied": cells[2].find("span").get_text(),
-		"icu_await_admit": cells[3].find("span").get_text(),
-		"icu_await_dc": cells[4].find("span").get_text(),
-		"hdu_empty": cells[5].find("span").get_text(),
-		"hdu_occupied": cells[6].find("span").get_text(),
-		"hdu_await_admit": cells[7].find("span").get_text(),
-		"hdu_await_discharge": cells[8].find("span").get_text(),
-		"hdu_min_icu_equiv": cells[9].find("span").get_text(),
-		"updated": cells[10].find("span").get("data-tooltip")
-	}
-	bedstate.append(hospital_state)
 
-with open('bedstate.json', 'w') as outfile:
-	json.dump(bedstate, outfile)
+###Static Values
+def vic_static():
+	for hospital in hospitals:
+		cells = hospital.find_all("td")
+		if cells[0].parent['class'][0] == "care-Tertiary":
+			tertiary, metro, private = 1, 1, 0
+		elif cells[0].parent['class'][0] == "care-SubTertiary":
+			tertiary, metro, private = 0, 1, 0
+		elif cells[0].parent['class'][0] == "care-Regional":
+			tertiary, metro, private = 0, 0, 0
+		elif cells[0].parent['class'][0] == "care-Private":
+			tertiary, metro, private = 0, 0, 1
+		hospital_state = {
+			"title": cells[0].find("span").get_text(),
+			"tertiary": tertiary,
+			"metro": metro,
+			"private": private,
+			"area": "vic",
+		}
+		hospital_static.append(hospital_state)
+	with open('hospital_static.json', 'w') as outfile:
+		json.dump(hospital_static, outfile)
+
+
+###Dynamic Values
+def vic_dynamic():
+	for hospital in hospitals:
+		cells = hospital.find_all("td")
+		title = cells[0].find("span").get_text(),
+		icu_emp = cells[1].find("span").get_text(),
+		icu_occ = cells[2].find("span").get_text(),
+		icu_aw_adm = cells[3].find("span").get_text(),
+		icu_aw_dc = cells[4].find("span").get_text(),
+		hdu_emp = cells[5].find("span").get_text(),
+		hdu_occ = cells[6].find("span").get_text(),
+		hdu_aw_adm = cells[7].find("span").get_text(),
+		hdu_aw_dc = cells[8].find("span").get_text(),
+		min_icu_eqv = cells[9].find("span").get_text(),
+		updated = cells[10].find("span").get("data-tooltip"),
+		ts = str(dtg)
+#		hospital_state = [title, icu_emp, icu_occ, icu_aw_adm, icu_aw_dc, hdu_emp, hdu_occ, hdu_aw_adm, hdu_aw_dc, min_icu_eqv, updated, ts]
+
+		hospital_state = {
+			"title": title,
+			"icu_emp": icu_emp,
+			"icu_occ": icu_occ,
+			"icu_aw_adm": icu_aw_adm,
+			"icu_aw_dc": icu_aw_dc,
+			"hdu_emp": hdu_emp,
+			"hdu_occ": hdu_occ,
+			"hdu_aw_adm": hdu_aw_adm,
+			"hdu_aw_dc": hdu_aw_dc,
+			"min_icu_eqv": min_icu_eqv,
+			"updated": updated,
+			"ts": ts
+		}
+		with open('hospitalstate.json', 'w') as outfile:
+			json.dump(hospital_state, outfile)
+		bedstate.append(hospital_state)
+
+
+	with open('bedstate.json', 'w') as outfile:
+		json.dump(bedstate, outfile)
+
+vic_dynamic()
+
+
+
+
+##Upload to Database
 
 
 ##Victorian COVID-19
